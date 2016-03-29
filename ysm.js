edbb2d89a6d4ef1c5ac9d33e6ac7f688a5c1d1ce
jku <jyrki.kujanpaa@gmail.com>
2016-03-29 18:11:52 +0300

    hsm tests, first version

--- ysm.js
+++ ysm.js
@@ -1,5 +1,16 @@
 
 const yfsm = require('./lib/yfsm');
+const yhsm = require('./lib/yhsm');
 
-module.exports.yState = yfsm.yState;
+/*
+ *  Flat state machine
+ */
+module.exports.yFState = yfsm.yState;
 module.exports.yFsm = yfsm.yFsm;
+
+/*
+ *  Hierarchial state machine
+ */
+module.exports.yEvent = yhsm.yEvent;
+module.exports.yHState = yhsm.yState;
+module.exports.yHsm = yhsm.yHsm;
